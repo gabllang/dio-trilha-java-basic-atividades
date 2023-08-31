a34ee06b9d2dfd47430f00bdec04672bddf8030a
Gabriel <davgabie@gmail.com>
2023-08-31 15:19:56 -0300

    terminado o desafio de sintaxe

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -6,17 +6,32 @@ public class ContaTerminal {
         
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
             
-            System.out.println("Por favor, digite o seu número");
+        System.out.println("Olá, bem vindo ao atendimento automatico da DGBank! ");
+            System.out.println("Por favor, digite o seu número: ");
             int numero = scanner.nextInt();
 
-            System.out.println("Por favor, digite o número da agência!");
-            String agencia = scanner.next();
+            scanner.nextLine();
 
-            System.out.println("Por favor, digite seu nome:");
-            String cliente = scanner.next();
+            System.out.println("Por favor, digite o número da agência: ");
+            String agencia = scanner.nextLine();
 
-            double saldo = 120;
+            System.out.println("Por favor, digite o seu nome: ");
+            String nomeCliente = scanner.nextLine();
 
-            System.out.println("Olá " + cliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está dispobível para saque");
+            // não sei se era para botar o saldo como questionario também eu não
+            // mas imaginei que não fosse, pois ficaria menos "realista", mas caso for par botar, aqui está o codigo
+            /*
+             *          System.out.print("Por favor, digite o saldo da conta: ");
+             *           double saldo = scanner.nextDouble();
+             * 
+            */
+            double saldo = 350.00;
+            // minha fatura da nubank kkkkkkkkk
+
+            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+                ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        // ouvi de um amigo que é uma boa pratica fechar o scanner quando acabo de usá-lo, por varias razões
+        scanner.close();
     }
 }
