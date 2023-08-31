aa76c2293f7120d62bb896e096bdb03d2123b831
Gabriel <davgabie@gmail.com>
2023-08-31 15:23:13 -0300

    finalizado desafio dio de sintaxe

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -5,7 +5,11 @@ public class ContaTerminal {
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-            
+        
+        // só pra deixar um espaço em branco e ficar esteticamente mais agradavel
+        System.out.println(" ");
+        System.out.println(" ");
+
         System.out.println("Olá, bem vindo ao atendimento automatico da DGBank! ");
             System.out.println("Por favor, digite o seu número: ");
             int numero = scanner.nextInt();
@@ -27,6 +31,9 @@ public class ContaTerminal {
             */
             double saldo = 350.00;
             // minha fatura da nubank kkkkkkkkk
+
+            System.out.println(" ");
+            System.out.println(" ");
 
             System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
                 ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
